c5e5128072a81bec4ec34be168bbd785a80b0d0b
striderwalk <striderwalk@gmail.com>
2025-03-10 11:04:48 +0000

    Added file selection from the command line. The filepath is added as a command line argument

--- app.py
+++ app.py
@@ -1,5 +1,25 @@
+import argparse
+import os
+import parser
+
+
+def get_filepath():
+    arg_parser = argparse.ArgumentParser(description="HTML parser")
+    arg_parser.add_argument("filepath", type=str, help="The file to parse")
+
+    args = arg_parser.parse_args()
+
+    if not os.path.isfile(args.filepath):
+        print(f"The file '{args.filepath}' could not be found.")
+        exit()
+    return args.filepath
+
+
 def main():
-    print("Hello world!")
+    filepath = get_filepath()
+    fileparser = parser.HTMLparser(filepath)
+
+    print(fileparser)
 
 
 if __name__ == "__main__":
