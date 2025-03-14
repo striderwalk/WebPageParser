b91241ea0343fc2171aa7bf82bb58421c6d98ff4
striderwalk <striderwalk@gmail.com>
2025-03-14 18:43:29 +0000

    maybe

--- app.py
+++ app.py
@@ -181,7 +181,7 @@ def download_file():
         root_file_name = "output"
 
         file_path = f"{root_file_name}_lengths.png"
-        return f"{len(word_data['labels'])=} {len(word_data['data'])=}"
+        return f"{(word_data['labels'])=} {(word_data['data'])=}"
 
         try:
             plt.bar(word_data["labels"], word_data["data"])
